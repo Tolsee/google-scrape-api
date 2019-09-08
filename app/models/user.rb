560305372d5e21a3e2e55f409d0521425df00f4e
Tolsee <tolsee3@gmail.com>
2019-09-08 13:21:26 +0545

    Create keyword, link model. Add relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+
+
+  has_many :keyword_users, dependent: :destroy
+  has_many :keywords, through: :keyword_users
 end
