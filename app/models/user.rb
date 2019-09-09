b9f93623e268c257ce37b3c46d914996325ef49c
Tolsee <tolsee3@gmail.com>
2019-09-10 00:08:55 +0545

    Cleanup and reverse_of

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
-
 
   has_many :keyword_users, dependent: :destroy
   has_many :keywords, through: :keyword_users
