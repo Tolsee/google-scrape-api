a8744cd823c6f120773fa2b8267eff15f5147665
Tolsee <tolsee3@gmail.com>
2019-09-10 00:35:01 +0545

    change total results to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_091701) do
+ActiveRecord::Schema.define(version: 2019_09_09_184327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2019_09_07_091701) do
 
   create_table "keywords", force: :cascade do |t|
     t.string "term", null: false
-    t.decimal "total_results"
+    t.integer "total_results"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["term"], name: "index_keywords_on_term"
