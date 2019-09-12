5f7364076364379da6d96a664619cd7d605cbc15
Tolsee <tolsee3@gmail.com>
2019-09-12 19:16:14 +0545

    Model type mappper

--- app/models/link/base.rb
+++ app/models/link/base.rb
@@ -3,5 +3,10 @@ module Link
     self.table_name = 'links'
 
     belongs_to :keyword
+
+    TYPE_MAPPER = {
+      'Link::Ad': 'ad_link',
+      'Link::Result': 'result_link'
+    }.with_indifferent_access.freeze
   end
 end
