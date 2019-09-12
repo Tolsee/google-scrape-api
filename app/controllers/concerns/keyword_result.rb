c2856a52c2399dc3176b45279ab1a158610b606f
Tolsee <tolsee3@gmail.com>
2019-09-12 19:17:05 +0545

    Update serialization

--- app/controllers/concerns/keyword_result.rb
+++ app/controllers/concerns/keyword_result.rb
@@ -5,9 +5,10 @@ module KeywordResult
     relations.each_with_object(HashWithIndifferentAccess.new) do |relation, agg|
       current_keyword_id = relation.id
       current_keyword_hash = agg[current_keyword_id] || {}
+      type = Link::Base::TYPE_MAPPER[relation.type]
       agg[current_keyword_id] = current_keyword_hash.merge(
         term: relation.term,
-        "#{relation.type}": relation.count,
+        "#{type}_count": relation.count,
         total_results: relation.total_results
       )
     end
